965fb773ff3a381931814214c9b7f5296f6439ba
Nick McCauley <busted_106@hotmail.com>
2023-03-01 21:24:42 +1100

    controllers are set up

---
+++ backend/controllers/songController.js
@@ -0,0 +1,90 @@
+const Song = require("../models/songModel");
+const mongoose = require("mongoose");
+
+// GET all songs
+const getAllSongs = async (req, res) => {
+  const songs = await Song.find({});
+
+  res.status(200).json(songs);
+};
+
+// Get ALL Indie Genre songs
+const getIndieRandom = async (req, res) => {
+  const songs = await Song.aggregate([
+    { $match: { genre: "Indie" } },
+    { $sample: { size: 5 } },
+  ]);
+
+  res.status(200).json(songs);
+};
+
+// GET single song
+const getOneSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No Such Song" });
+  }
+
+  const song = await Song.findById(id);
+
+  if (!song) {
+    return res.status(404).json({ error: "No Such Song" });
+  }
+  res.status(200).json(song);
+};
+
+// POST a new song
+const createSong = async (req, res) => {
+  const { artist, songName, genre } = req.body;
+
+  // add doc to DB
+  try {
+    const song = await Song.create({ artist, songName, genre });
+    res.status(200).json(song);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE a song
+const deleteOneSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No Such Song" });
+  }
+
+  const song = await Song.findOneAndDelete({ _id: id });
+
+  if (!song) {
+    return res.status(404).json({ error: "No Such Song" });
+  }
+  res.status(200).json(song);
+};
+// UPDATE a song
+const updateSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No Such Song" });
+  }
+
+  const song = await Song.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  res.status(200).json(song);
+};
+
+module.exports = {
+  createSong,
+  getAllSongs,
+  getOneSong,
+  deleteOneSong,
+  updateSong,
+  getIndieRandom,
+};
--- backend/routes/songs.js
+++ backend/routes/songs.js
@@ -1,38 +1,31 @@
 const { Router } = require("express");
 const express = require("express");
-const Song = require("../models/songModel");
-
+const {
+  createSong,
+  getAllSongs,
+  getOneSong,
+  deleteOneSong,
+  updateSong,
+  getIndieRandom,
+} = require("../controllers/songController");
 const router = express.Router();
 
 // GET all songs
-router.get("/", (req, res) => {
-  res.json({ message: "GET all songs" });
-});
+router.get("/", getAllSongs);
+
+// Get 5 random Indie Songs
+router.get("/Indie", getIndieRandom);
 
 // GET single song
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET one song" });
-});
+router.get("/:id", getOneSong);
 
 // POST a new song
-router.post("/", async (req, res) => {
-  const { artist, songName, genre } = req.body;
-  try {
-    const song = await Song.create({ artist, songName, genre });
-    res.status(200).json(song);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createSong);
 
 // DELETE a song
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a song" });
-});
+router.delete("/:id", deleteOneSong);
 
 // UPDATE a workout
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a song" });
-});
+router.patch("/:id", updateSong);
 
 module.exports = router;
