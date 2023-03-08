fd5060150f958f0986d204011c20263d4c1ae345
pali-rojas <paloma.rkhn@gmail.com>
2023-03-08 23:18:04 +1100

    fetch functions to frontend added to comp checkbox

--- frontend/src/components/Checkbox.jsx
+++ frontend/src/components/Checkbox.jsx
@@ -3,21 +3,21 @@ import {useNavigate} from "react-router-dom"
 import "./Checkbox.css";
 import NoSelectionAlert from "./NoSelectionAlert";
 
-import {rockSongs,
-  hiphopSongs,
-  popSongs,
-  punkSongs,
-  metalSongs, 
-  classicalSongs,
-  electronicSongs,
-  reggaetonSongs,
-  indieSongs,
-  seventiesSongs,
-  eightiesSongs,
-  ninetiesSongs,
-  twothousandsSongs,
-  twentytenSongs,
-  twentytwentySongs} from "../../../backend/controllers/checkboxController";
+import {RockSongs,
+  HipHopSongs,
+  PopSongs,
+  PunkSongs,
+  MetalSongs, 
+  ClassicalSongs,
+  ElectronicSongs,
+  ReggaetonSongs,
+  IndieSongs,
+  SeventiesSongs,
+  EightiesSongs,
+  NinetiesSongs,
+  TwothousandsSongs,
+  TwentytenSongs,
+  TwentytwentySongs} from "./fetchSongs";
 
 const Checkbox = ({ id, label, onChange }) => {
   return (
@@ -36,21 +36,21 @@ const App = () => {
   const navigate = useNavigate();
 
   const checkboxes = [
-    { id: 1, label: "Rock", onSelected: rockSongs },
-    { id: 2, label: "Hip Hop", onSelected: hiphopSongs },
-    { id: 3, label: "Pop", onSelected: popSongs },
-    { id: 4, label: "Punk", onSelected: punkSongs },
-    { id: 5, label: "Metal", onSelected: metalSongs },
-    { id: 6, label: "Classical", onSelected: classicalSongs },
-    { id: 7, label: "Electronic", onSelected: electronicSongs },
-    { id: 8, label: "Reggaeton", onChaonSelectednge: reggaetonSongs },
-    { id: 9, label: "Indie", onSelected: indieSongs },
-    { id: 10, label: "70s", onSelected: seventiesSongs },
-    { id: 11, label: "80s", onSelected: eightiesSongs },
-    { id: 12, label: "90s", onSelected: ninetiesSongs },
-    { id: 13, label: "2000s", onSelected: twothousandsSongs },
-    { id: 14, label: "2010s", onSelected: twentytenSongs },
-    { id: 15, label: "2020s", onSelected: twentytwentySongs }
+    { id: 1, label: "Rock", onSelected: RockSongs },
+    { id: 2, label: "Hip Hop", onSelected: HipHopSongs },
+    { id: 3, label: "Pop", onSelected: PopSongs },
+    { id: 4, label: "Punk", onSelected: PunkSongs },
+    { id: 5, label: "Metal", onSelected: MetalSongs },
+    { id: 6, label: "Classical", onSelected: ClassicalSongs },
+    { id: 7, label: "Electronic", onSelected: ElectronicSongs },
+    { id: 8, label: "Reggaeton", onChaonSelectednge: ReggaetonSongs },
+    { id: 9, label: "Indie", onSelected: IndieSongs },
+    { id: 10, label: "70s", onSelected: SeventiesSongs },
+    { id: 11, label: "80s", onSelected: EightiesSongs },
+    { id: 12, label: "90s", onSelected: NinetiesSongs },
+    { id: 13, label: "2000s", onSelected: TwothousandsSongs },
+    { id: 14, label: "2010s", onSelected: TwentytenSongs },
+    { id: 15, label: "2020s", onSelected: TwentytwentySongs }
   ];
 
   const handleChange = (e) => {
