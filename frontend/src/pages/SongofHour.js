32add1fcbf24ecb6f45bd63076faad9c795d4c7e
Frank chapman <fchapman_e3@hotmkail.com>
2023-03-09 10:40:21 +1100

    adding songofhour

--- frontend/src/pages/SongofHour.js
+++ frontend/src/pages/SongofHour.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react';
+import VideoPlayer from '../components/VideoPlayer';
+import './SongofHour.css';
 
-// import components
+const App = () => {
+  return (
+    <div>
+      <h1>Video content that's always in sync with your day</h1>
+      <h2>Join the Revolution with Ramdomfy </h2>
+      <VideoPlayer autoPlay={true}/>
+    </div>
+  );
+};
 
-function Home() {
-  return <div className="Home"></div>;
-}
-
-export default Home;
+export default App;
