179037832f768a701f382cdc4c78e06ff68e4007
Nick McCauley <busted_106@hotmail.com>
2023-03-08 20:19:27 +1100

    fixing imports

--- frontend/src/components/Api.jsx
+++ frontend/src/components/Api.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import SongDetails from "./SongDetails";
 
 function Home() {
   const [songs, setSongs] = useState(null);
