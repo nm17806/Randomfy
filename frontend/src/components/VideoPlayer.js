379975a78191dc982c64d8de711e349757f8ac41
Nick McCauley <busted_106@hotmail.com>
2023-03-15 22:13:27 +1100

    change YT videos

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,50 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import YouTube from 'react-youtube';
-import '../pages/SongofHour.css';
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import "../pages/SongofHour.css";
 
-///////////// asignar horario para reprodcucion de musica //////////// 
+///////////// asignar horario para reprodcucion de musica ////////////
 ////////// revisar codigo is playing para reprodcucion auto (1 warning)////
 function VideoPlayer() {
-  const [timeOfDay, setTimeOfDay] = useState('');
+  const [timeOfDay, setTimeOfDay] = useState("");
   const [isPlaying, setIsPlaying] = useState(true);
 
   useEffect(() => {
     const date = new Date();
     const hour = date.getHours();
     if (hour < 12) {
-      setTimeOfDay('morning');
+      setTimeOfDay("morning");
     } else if (hour >= 12 && hour < 18) {
-      setTimeOfDay('afternoon');
+      setTimeOfDay("afternoon");
     } else {
-      setTimeOfDay('night');
+      setTimeOfDay("night");
     }
   }, []);
-//////////////// ID de los videos youtube////////////////
-  const videosMorning = [
-    'SuCyZlksrI',
-    'hlWiI4xVXKY',
-    'iqmO1RlqorU'
-  ];
+  //////////////// ID de los videos youtube////////////////
+  const videosMorning = ["9UMxZofMNbA", "hlWiI4xVXKY", "iqmO1RlqorU"];
   ///////// pendiente modificae ID youtube elegir videos para la tarde y la noche
-  const videosAfternoon = [
-    'SuCyZlksrI',
-    'hlWiI4xVXKY',
-    'iqmO1RlqorU'
-  ];
-  const videosNight = [
-    'SSuCyZlksrI',
-    'hlWiI4xVXKY',
-    'iqmO1RlqorU'
-  ];
-//////////////Codigo para que sean random los videos////////////
+  const videosAfternoon = ["IUl7su3j1Eo&ab", "L8hPtjGb3R0", "2ig28K45I90"];
+  const videosNight = ["GawAvQTYVCE", "Wx55dpPMVX0", "kKaF-sjG06g"];
+  //////////////Codigo para que sean random los videos////////////
   const getRandomVideo = (videos) => {
     return videos[Math.floor(Math.random() * videos.length)];
   };
 
   const getVideoSource = () => {
-    if (timeOfDay === 'morning') {
+    if (timeOfDay === "morning") {
       return getRandomVideo(videosMorning);
-    } else if (timeOfDay === 'afternoon') {
+    } else if (timeOfDay === "afternoon") {
       return getRandomVideo(videosAfternoon);
     } else {
       return getRandomVideo(videosNight);
@@ -60,14 +48,14 @@ function VideoPlayer() {
   };
 
   const opts = {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
     playerVars: {
       autoplay: 1,
-      mute: 0,// cambiar de 1 a 0 para quitar el mute
+      mute: 0, // cambiar de 1 a 0 para quitar el mute
       loop: 1,
-      controls: 1
-    }
+      controls: 1,
+    },
   };
 
   return (
