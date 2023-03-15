5abedd3ce4909949a5133fb3bd324d63f896cb45
Nick McCauley <busted_106@hotmail.com>
2023-03-15 21:46:47 +1100

    Adding a Login

--- frontend/src/components/Navbar1.jsx
+++ frontend/src/components/Navbar1.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function Navbar1() {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <Navbar bg="light" expand="sm">
       <Container>
@@ -15,6 +24,27 @@ function Navbar1() {
             <Nav.Link href="/songofthehour">Song of the Hour</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        {user && (
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <div>
+                <span>{user.email}</span>
+                <button onClick={handleClick}>Log out</button>
+              </div>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        )}
+
+        {!user && (
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <a href="/login">Login</a>
+              <br></br>
+              <a href="/signup">Sign Up</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        )}
       </Container>
     </Navbar>
   );
